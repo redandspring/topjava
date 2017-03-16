daa5bb7394f335d67e30305441e99aad07bf2d31
taad2@komus.net <taad2@komus.net>
2017-03-16 12:51:34 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -27,9 +27,15 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+        List<UserMealWithExceed> userMealWithExceeds = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        userMealWithExceeds.stream().forEach(System.out::println);
+
+        // реализация через циклы
+        System.out.println();
+        System.out.println("Реализация через циклы:");
+        List<UserMealWithExceed> userMealWithExceeds_FOREACH = getFilteredWithExceededOLD(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        userMealWithExceeds_FOREACH.stream().forEach(System.out::println);
+
     }
 
 
